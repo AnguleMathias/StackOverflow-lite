834fdfd664c1203f7ebad9f78b2f0e3c1637d982
Angule Mathias <angulemathias3@gmail.com>
2018-09-22 11:51:56 +0300

    [FEATURE #160564566]- User login

--- static/js/authentication.js
+++ static/js/authentication.js
@@ -37,3 +37,43 @@ const register = (event) => {
 };
 
 document.getElementById('register_form').addEventListener('submit', register);
+
+/*---------------------------------------------------
+
+Login user
+
+----------------------------------------------------*/
+const login = (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const inputData = {
+        username,
+        password,
+    };
+
+    fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify(inputData),
+    })
+
+        .then(response => response.json())
+        .then((data) => {
+            if (data.message === 'Successful login') {
+                localStorage.setItem('access_token', data.access_token);
+                localStorage.setItem('username', data.username);
+                window.location.replace('questions.html');
+            }
+            if (data.message) {
+                showAlert(data.message);
+            }
+        })
+        .catch(error => showAlert(error));
+};
+
+document.getElementById('login_form').addEventListener('submit', login);
