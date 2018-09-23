22c4aee6fa3f299e24a5320ae441d4d4e45d4cac
Angule Mathias <angulemathias3@gmail.com>
2018-09-23 15:01:09 +0300

    [FEATURE #160608490]- Get answers to question add date and owner

--- static/js/questionAnswers.js
+++ static/js/questionAnswers.js
@@ -13,7 +13,7 @@ const getOneQuestion = () => {
                 method: 'GET',
                 headers: {
                     'Content-type': 'application/json',
-                    'Authorization': 'Bearer ' + localStorage.getItem('access_token'),
+                    Authorization: 'Bearer ' + localStorage.getItem('access_token'),
                 },
             })
             .then(response => response.json())
@@ -21,20 +21,34 @@ const getOneQuestion = () => {
                 if (data.message !== 'success') {
                     showAlert(data.message);
                 } else if (data.message === 'success') {
+                    console.log(data);
+                    const split = data['Question Details'].date.split(',')[1];
+                    const split1 = split.split(' ');
+                    const day = split1[1];
+                    const month = split1[2];
+                    const year = split1[3];
                     const output = `<div class="heading"><h3 class="heading-25">${data['Question Details'].title}</h3>
                                 <div class="clear"></div></div><div class="divider"></div><div class="heading"><div>
                                 <p>${data['Question Details'].question}</p>
                                 <a href="#" class="tag">tag 1</a> <a href="#" class="tag">tag 2</a>
-                                <span class="float-right"><i>Posted by <span class="text-primary"> ${data['Question Details'].qstn_owner}`;
+                                <span class="float-right"><i>Posted by <span class="text-primary"> ${data['Question Details'].qstn_owner}
+                                </span></i> on <span class="text-primary"> ${day}-${month}-${year}</span></span>`;
 
                     document.getElementById('question_selected').innerHTML = output;
                 }
                 let answers = '';
                 if (data.Answers) {
                     data.Answers.forEach((element) => {
+                        const split = element.date.split(',')[1];
+                        const split1 = split.split(' ');
+                        const day = split1[1];
+                        const month = split1[2];
+                        const year = split1[3];
                         answers += `<div class="numbers small"><div>2</div> <div class="img-accept">
                                 <img src="../static/images/tick.jpg" class="img-responsive"> </div> </div>
-                                <div class="question-summaries large"> <p>${element.answer}</p> </div> </div>
+                                <div class="question-summaries large"> <p>${element.answer}</p> 
+                                <span class="float-right"><i>Answer by <span class="text-primary"> ${element.ans_owner}
+                                </span></i> on <span class="text-primary"> ${day}-${month}-${year}</span></span></div> </div>
                                 <div class="clear divider"></div> <div class="clear"></div>`;
                     });
                     document.getElementById('ans_to_qst').innerHTML = answers;
