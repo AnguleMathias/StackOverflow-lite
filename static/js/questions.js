bc3c922dd175a129da7ed7cb1f73a7fe6c6504c0
Angule Mathias <angulemathias3@gmail.com>
2018-09-23 18:30:40 +0300

    [FEATURE #160581448]- View specific user questions

--- static/js/questions.js
+++ static/js/questions.js
@@ -48,16 +48,76 @@ const getQuestions = () => {
         })
         .catch(error => showAlert(error));
 };
-
 /*------------------------------------------------
 
-store qstn id in localStorage
+store qstn id in localStorage for all questions
 
 --------------------------------------------------*/
-
 const setOneQuestion = (qstn_id) => {
     localStorage.setItem('qstn_id', qstn_id);
     window.location.replace('templates/answer.html');
+};
+/*--------------------------------------------
+
+View specific user questions
+
+----------------------------------------------*/
+
+const getUserQuestions = () => {
+    fetch('http://127.0.0.1:5000/api/v1/questions/user_questions', {
+
+        method: 'GET',
+
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            Authorization: 'Bearer ' + localStorage.getItem('access_token')
+        },
+    })
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data.message)
+            const questions = data.questions;
+            let output = '';
+            for (let counter = 0; counter < questions.length; counter++) {
+                const question = questions[counter]['question'];
+                const qstn_id = questions[counter]['qstn_id'];
+                const qstn_owner = questions[counter]['qstn_owner'];
+                const split = questions[counter]['date'].split(',')[1];
+                const split1 = split.split(' ');
+                const day = split1[1];
+                const month = split1[2];
+                const year = split1[3];
+
+                output += `<div class="question-row">
+                                <div class="question-summaries padding-16">
+                                    <a onclick="setOneUserQuestion(${qstn_id})" class="question"> ${question}</a>
+                                    <a href="#" class="tag">tag 1</a> <a href="#" class="tag">tag 2</a>
+                                    <span class="float-right"><i>Posted by <span class="text-primary"> ${qstn_owner}
+                                    </span></i> on <span class="text-primary"> ${day}-${month}-${year}</span></span>
+                                </div>
+                                <div class="numbers">
+                                    <div class="number-slot"><label>2</label><label>votes</label></div>
+                                    <div class="number-slot"><label>4</label><label>answers</label></div>
+                                    <div class="number-slot"><label>13</label><label>views</label></div>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                                <div class="clear"></div><div class="divider light"></div>`;
+            }
+            document.getElementById('user_questions').innerHTML = output;
+        })
+        .catch(error => showAlert(error));
+};
+
+/*------------------------------------------------
+
+store qstn id in localStorage for my questions
+
+--------------------------------------------------*/
+
+const setOneUserQuestion = (qstn_id) => {
+    localStorage.setItem('qstn_id', qstn_id);
+    window.location.replace('answer.html');
 };
 
 /*------------------------------------------------
